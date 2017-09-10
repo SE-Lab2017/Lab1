1b2b469de0365ad0a8f19a0ba1fb8ea395d3e7c2
xy <424350103@@qq.com>
2017-09-10 22:47:23 +0800

    added ShowDirectedGraph interface

--- src/org/yoooo/se1/ShowDirectedGraph.java
+++ src/org/yoooo/se1/ShowDirectedGraph.java
@@ -8,6 +8,5 @@ public class ShowDirectedGraph {
      * @param filename filename of generated picture(without extension)
      */
     public static void showDirectedGraph(SimpleDirectedWeightGraph<String, Integer> graph, String filename) {
-
     }
 }
