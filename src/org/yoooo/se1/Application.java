b9a0bd17d0b57b83342616fc5c8037c01bdd1a9d
Moandor <moandor.y@gmail.com>
2017-09-11 12:36:35 +0800

    Add graph generation

--- src/org/yoooo/se1/Application.java
+++ src/org/yoooo/se1/Application.java
@@ -1,5 +1,7 @@
 package org.yoooo.se1;
 
+import java.util.Scanner;
+
 public class Application {
     public void run(String[] args) {
 
@@ -16,5 +18,41 @@ public class Application {
     }
 
     private Application() {
+    }
+
+    /**
+     * Parses string and generates graph from the string as specified in feature 1. The string
+     * should only contain lowercase letters and spaces.
+     *
+     * @param string string from input file
+     * @return graph created from string
+     */
+    private SimpleDirectedWeightGraph<String, Integer> stringToGraph(String string) {
+        Scanner scanner = new Scanner(string);
+        SimpleDirectedWeightGraph<String, Integer> graph = new SimpleDirectedWeightGraph<>(
+                new EdgeFactory<String, Integer>() {
+                    int next;
+
+                    @Override
+                    public Integer createEdge(String source, String target) {
+                        return next++;
+                    }
+                });
+        if (!scanner.hasNext()) {
+            return graph;
+        }
+        String prev = scanner.next();
+        graph.addVertex(prev);
+        while (scanner.hasNext()) {
+            String vertex = scanner.next();
+            graph.addVertex(vertex);
+            Integer edge = graph.getEdge(prev, vertex);
+            if (edge != null) {
+                graph.setEdgeWeight(edge, graph.getEdgeWeight(edge) + 1);
+            } else {
+                graph.addEdge(prev, vertex);
+            }
+        }
+        return graph;
     }
 }
